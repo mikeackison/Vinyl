ebce7df090dd954844da51136235d9e6c4784e6b
Michael Ackison <michaelackison@Michaels-MacBook-Air.local>
2020-10-28 19:33:26 -0400

    Updated from vinyl2

--- vinyl.rb
+++ vinyl.rb
@@ -8,20 +8,18 @@ class Vinyl
     def read_artist
         @artist
     end
-    def read_album
-        @album
-    end
-    def read_songs
-        @tracklist
-    end
     def shuff_songs
-        @tracklist.sort_by{rand}
+        @tracklist.shuffle
     end
-    def shuff_artisty 
-        @artist.chars.sort_by{rand}.join
-        # @artist = @artist.chars.shuffle.join
+    def shuff_artist
+        @artist = @artist.chars.shuffle.join
+    end
+    def get_track(number)
+        @tracklist[number - 1]
     end
 end
+
+# .chars.shuffle.join
 
 class VinylCollection
     def initialize(collection)
@@ -30,12 +28,8 @@ class VinylCollection
     def artist_lookup(name)
         correct_name = nil
         @collection.each do |artist|
-            print "Comparing artist "
-            print artist.inspect
-            print " against name #{name}\n"
             if artist.read_artist == name
                 correct_name = artist
-                puts "name matched, setting correct_name '#{name}' to current contact"
             end
         end
        correct_name
@@ -43,10 +37,10 @@ class VinylCollection
 end
 
 
-vinyl = VinylCollection.new([
+vinyl_collection = VinylCollection.new([
     Vinyl.new("Prince and the Revolution", "1999", 
-
-            ["1999",
+            [
+            "1999",
             "Little Red Corvette",
             "Delirious",
             "Let's Pretend We're Married",
@@ -58,9 +52,10 @@ vinyl = VinylCollection.new([
             "All the Critics Love U in New York",
             "International Lover"]),
     
-    Vinyl.new("Soulwax", "Any Minute Now", 
-    
-            ["E Talking",
+    Vinyl.new(
+            "Soulwax", "Any Minute Now", 
+            [
+            "E Talking",
             "Any Minute Now",
             "Please... Don't Be Yourself",
             "Compute",
@@ -72,19 +67,57 @@ vinyl = VinylCollection.new([
             "Miserable Girl",
             "YYY/NNN",
             "The Truth Is So Boring",
-            "Dance 2 Slow"])
+            "Dance 2 Slow"
+        ])
 ])
 
+## Common test SETUP
 
-puts vinyl.inspect
-name = "Prince and the Revolution"
+### TEST 1 GOES HERE
+puts "TEST 1"
+name = "Soulwax"
+vinyl = vinyl_collection.artist_lookup(name)
 
-puts vinyl.artist_lookup(name).inspect
+original_title = vinyl.read_artist
+vinyl.shuff_artist
+new_title = vinyl.read_artist
 
-puts nil
+if new_title.class == original_title.class && new_title != original_title
+    puts "program is correct"
+else
+    puts "ERROR: titles should have been different"
+    puts "original title: #{original_title.inspect}"
+    puts "new title: #{new_title.inspect}"
+end
 
-puts vinyl.artist_lookup(name).shuff_songs.inspect
+### TEST 2 GOES HERE
+puts "TEST 2"
+# name = "Soulwax"
+# vinyl = vinyl_collection.artist_lookup(name)
 
-puts vinyl.artist_lookup(name).shuff_artist.inspect
+original_title = vinyl.read_artist
+new_title = vinyl.read_artist
 
+if new_title.class == original_title.class && new_title == original_title
+    puts "program is correct"
+else
+    puts "ERROR: titles should have been the same"
+    puts "original title: #{original_title.inspect}"
+    puts "new title: #{new_title.inspect}"
+end
 
+### TEST 3 GOES HERE
+puts "TEST 3"
+# name = "Soulwax"
+# vinyl = vinyl_collection.artist_lookup(name)
+
+actual_title = vinyl.get_track(9)
+expected_title = "NY Excuse"
+
+if actual_title.class == expected_title.class && actual_title == expected_title
+    puts "program is correct"
+else
+    puts "ERROR: returned title doesn't match expectation"
+    puts "actual_title: #{actual_title.inspect}"
+    puts "expected_title: #{expected_title.inspect}"
+end
