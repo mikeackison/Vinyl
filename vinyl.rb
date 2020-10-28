93178386f07d2f3647dcb857248275dea0638525
Michael Ackison <michaelackison@Michaels-MacBook-Air.local>
2020-10-28 19:35:40 -0400

    add 3 mini-tests

--- vinyl.rb
+++ vinyl.rb
@@ -1,123 +1,123 @@
-
 class Vinyl
     def initialize(artist, album, tracklist)
-        @artist = artist
-        @album = album
-        @tracklist = tracklist
+      @artist = artist
+      @album = album
+      @tracklist = tracklist
     end
+  
     def read_artist
-        @artist
+      @artist
     end
+  
+    def read_songs
+      @tracklist
+    end
+  
     def shuff_songs
-        @tracklist.shuffle
+      @tracklist.shuffle
     end
+  
     def shuff_artist
-        @artist = @artist.chars.shuffle.join
+      @artist = @artist.chars.shuffle.join
     end
+  
     def get_track(number)
-        @tracklist[number - 1]
+      @tracklist[number - 1]
     end
-end
-
-# .chars.shuffle.join
-
-class VinylCollection
+  end
+  
+  # .chars.shuffle.join
+  
+  class VinylCollection
     def initialize(collection)
-        @collection = collection
+      @collection = collection
     end
+  
     def artist_lookup(name)
-        correct_name = nil
-        @collection.each do |artist|
-            if artist.read_artist == name
-                correct_name = artist
-            end
+      correct_name = nil
+      @collection.each do |artist|
+        if artist.read_artist == name
+          correct_name = artist
         end
-       correct_name
+      end
+      correct_name
     end
-end
-
-
-vinyl_collection = VinylCollection.new([
-    Vinyl.new("Prince and the Revolution", "1999", 
-            [
+  end
+  
+  require "minitest/autorun"
+  
+  class Test < Minitest::Test
+    def setup
+      @vinyl_collection = vinyl_collection = VinylCollection.new(
+        [
+          Vinyl.new(
+            "Prince and the Revolution",
             "1999",
-            "Little Red Corvette",
-            "Delirious",
-            "Let's Pretend We're Married",
-            "D.M.S.R.",
-            "Automatic",
-            "Something in the Water (Does Not Compute)",
-            "Free",
-            "Lady Cab Driver",
-            "All the Critics Love U in New York",
-            "International Lover"]),
-    
-    Vinyl.new(
-            "Soulwax", "Any Minute Now", 
             [
-            "E Talking",
+              "1999",
+              "Little Red Corvette",
+              "Delirious",
+              "Let's Pretend We're Married",
+              "D.M.S.R.",
+              "Automatic",
+              "Something in the Water (Does Not Compute)",
+              "Free",
+              "Lady Cab Driver",
+              "All the Critics Love U in New York",
+              "International Lover",
+            ]
+          ),
+  
+          Vinyl.new(
+            "Soulwax",
             "Any Minute Now",
-            "Please... Don't Be Yourself",
-            "Compute",
-            "KracK",
-            "Slowdance",
-            "A Ballad to Forget",
-            "Accidents and Compliments",
-            "NY Excuse",
-            "Miserable Girl",
-            "YYY/NNN",
-            "The Truth Is So Boring",
-            "Dance 2 Slow"
-        ])
-])
-
-## Common test SETUP
-
-### TEST 1 GOES HERE
-puts "TEST 1"
-name = "Soulwax"
-vinyl = vinyl_collection.artist_lookup(name)
-
-original_title = vinyl.read_artist
-vinyl.shuff_artist
-new_title = vinyl.read_artist
-
-if new_title.class == original_title.class && new_title != original_title
-    puts "program is correct"
-else
-    puts "ERROR: titles should have been different"
-    puts "original title: #{original_title.inspect}"
-    puts "new title: #{new_title.inspect}"
-end
-
-### TEST 2 GOES HERE
-puts "TEST 2"
-# name = "Soulwax"
-# vinyl = vinyl_collection.artist_lookup(name)
-
-original_title = vinyl.read_artist
-new_title = vinyl.read_artist
-
-if new_title.class == original_title.class && new_title == original_title
-    puts "program is correct"
-else
-    puts "ERROR: titles should have been the same"
-    puts "original title: #{original_title.inspect}"
-    puts "new title: #{new_title.inspect}"
-end
-
-### TEST 3 GOES HERE
-puts "TEST 3"
-# name = "Soulwax"
-# vinyl = vinyl_collection.artist_lookup(name)
-
-actual_title = vinyl.get_track(9)
-expected_title = "NY Excuse"
-
-if actual_title.class == expected_title.class && actual_title == expected_title
-    puts "program is correct"
-else
-    puts "ERROR: returned title doesn't match expectation"
-    puts "actual_title: #{actual_title.inspect}"
-    puts "expected_title: #{expected_title.inspect}"
-end
+            [
+              "E Talking",
+              "Any Minute Now",
+              "Please... Don't Be Yourself",
+              "Compute",
+              "KracK",
+              "Slowdance",
+              "A Ballad to Forget",
+              "Accidents and Compliments",
+              "NY Excuse",
+              "Miserable Girl",
+              "YYY/NNN",
+              "The Truth Is So Boring",
+              "Dance 2 Slow",
+            ]
+          ),
+        ]
+      )
+  
+      name = "Soulwax"
+      @vinyl = @vinyl_collection.artist_lookup(name)
+    end
+  
+    def test_shuff_artist
+      original_title = @vinyl.read_artist
+      vinyl.shuff_artist
+      new_title = @vinyl.read_artist
+      refute_equal original_title, new_title
+      assert_equal(new_title.class, original_title.class)
+    end
+  
+    def test_new_title
+      original_title = @vinyl.read_artist
+      new_title = @vinyl.read_artist
+      assert_equal new_title.class, original_title.class
+      assert_equal new_title, original_title
+    end
+  
+    def test_expected_title
+      actual_title = @vinyl.get_track(9)
+      expected_title = "NY Excuse"
+      assert_equal actual_title.class, expected_title.class
+      assert_equal actual_title, expected_title
+  
+    end
+  
+    def vinyl
+      @vinyl
+    end
+  end
