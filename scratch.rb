1d0b5f882deb18dfaa8d28ed85b6d2424465f42c
mikeackison <44005561+mikeackison@users.noreply.github.com>
2020-11-05 08:54:11 -0500

    Delete scratch.rb

--- scratch.rb
+++
@@ -1,9 +0,0 @@
-❯ ruby vinyl.rb
-Traceback (most recent call last):
-vinyl.rb:47:in `<main>': uninitialized constant Minitest (NameError)
-
-
-1) Error:
-Test#test_expected_title:
-NameError: uninitialized constant Test::VinylCollection
-    vinyl_test.rb:5:in `setup'
