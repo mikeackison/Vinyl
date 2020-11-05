1557903bcfdc3ff84e6a50e63c48b08076e74d59
Michael Ackison <michaelackison@Michaels-MacBook-Air.local>
2020-11-04 20:02:22 -0500

    removed 'Joe Biden'

--- vinyl.rb
+++ vinyl.rb
@@ -42,24 +42,24 @@ class VinylCollection
   end
 end
 
-vinyl_collection = VinylCollection.new(
-[
-  Vinyl.new(
-    "Joe Biden",
-    "Democrats",
-    [
-      "Policy 1",
-      "Policy 2",
-    ]
-  ),
-  Vinyl.new(
-    "Orange",
-    "Republican",
-    [
-      "Policy 1",
-      "Policy 2",
-    ]
-  ),
-])
+# vinyl_collection = VinylCollection.new(
+# [
+#   Vinyl.new(
+#     "Joe Biden",
+#     "Democrats",
+#     [
+#       "Policy 1",
+#       "Policy 2",
+#     ]
+#   ),
+#   Vinyl.new(
+#     "Orange",
+#     "Republican",
+#     [
+#       "Policy 1",
+#       "Policy 2",
+#     ]
+#   ),
+# ])
 
-puts "Here are all the songs by Joe Biden: #{vinyl_collection.artist_lookup("Joe Biden").read_songs}"
+# puts "Here are all the songs by Joe Biden: #{vinyl_collection.artist_lookup("Joe Biden").read_songs}"
